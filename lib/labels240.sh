f5fae69d5f4120b2f8f644f9c7aec25eb306cd8d
mtovmassian <martin.tovmassian@protonmail.com>
2018-02-15 10:42:12 +0100

    centralize project aliases into single file

--- lib/labels240.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-labels240_home_dir="~/dev/labels-240"
-
-alias labels240="cd ${labels240_home_dir}"
-
-alias labels240:web="cd ${labels240_home_dir}/labels240-client"
-
-alias labels240:srv="cd ${labels240_home_dir}/labels240"
