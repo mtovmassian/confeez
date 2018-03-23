22f54ab64f9ce92fe8644fc60d28423f5a112664
mtovmassian <martin.tovmassian@protonmail.com>
2018-03-23 09:39:49 +0100

    boyscounting install script Double quote to prevent globbing and word splitting explicit escaping '\/' remove useless cat

--- install.sh
+++ install.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
-CONFEEZ_DIR=$(dirname $(readlink -f ${0}))
+CONFEEZ_DIR=$(dirname $(readlink -f "${0}"))
 VIMRC_FILE="${HOME}/.vimrc"
 
 link_vimrc() {
   if [[ -f "${VIMRC_FILE}" ]]; then rm "${VIMRC_FILE}"; fi
-  ln -s ${HOME}/confeez/.vimrc "${VIMRC_FILE}"
+  ln -s "${HOME}/confeez/.vimrc" "${VIMRC_FILE}"
 }
 
 init_confeez_file() {
-  confeez_dir_assignement=$(sed "s/\//\\\\\//g" <<< "CONFEEZ_DIR=\"${CONFEEZ_DIR}\"")
+  confeez_dir_assignement=$(sed "s/\\//\\\\\\//g" <<< "CONFEEZ_DIR=\"${CONFEEZ_DIR}\"")
   sed -i -e "s/CONFEEZ_DIR=.*/$confeez_dir_assignement/g" confeez.sh
 }
 
 source_confeez_file() {
   source_statement="source ${CONFEEZ_DIR}/confeez.sh"
-  statement_exist=$(cat "${HOME}/.bashrc" | grep "${source_statement}")
-  if [[ ${#statement_exist} -eq 0 ]]
+  statement_exists=$(grep "${source_statement}" < "${HOME}/.bashrc")
+  if [[ ${#statement_exists} -eq 0 ]]
   then
-    printf "\n%s\n" "${source_statement}" >> ${HOME}/.bashrc
+    printf "\\n%s\\n" "${source_statement}" >> "${HOME}/.bashrc"
   fi
 }
 
@@ -31,7 +31,7 @@ install() {
 echo "This script will update you current .bashrc, and overwrite your .vimrc"
 echo "Do you want to continue? Y/n"
 
-read user_choice
+read -r user_choice
 
 if [[ "${user_choice}" == Y ]]
 then
