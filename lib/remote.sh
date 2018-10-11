b65f4905c9ab3f5a7bd6960a797bf6ed219e6b93
Martin Tovmassian <martin.tovmassian@protonmail.com>
2018-10-11 11:47:08 +0200

    add ssh copy key func

--- lib/remote.sh
+++ lib/remote.sh
@@ -4,6 +4,14 @@ AWS_HARDIS_KEY="${HOME}/.safe/aws/hardis/HardisGroup_BA_Grenoble.pem"
 
 AWS_MARTIN_KEY="${HOME}/.safe/aws/martin/aws-martin-key.pem"
 
+copy_ssh_key() {
+ user_at_host="$1"
+ # ssh-copy-id ~/.ssh/id_rsa.pub "${user_at_host}"
+ cat ~/.ssh/id_rsa.pub | ssh "${user_at_host}" 'cat >> ~/.ssh/authorized_keys'
+}
+
+alias ssh:copykey="copy_ssh_key"
+
 alias ssh:sd="ssh usercds@192.168.32.187"
 
 alias ssh:se1="ssh usersmartexchange@192.168.38.154"
